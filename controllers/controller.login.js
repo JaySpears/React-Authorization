e5ccbef80e9dfd36c999ed8ee9b8dfcaef52e7c7
JoshSpears3 <josh.spears@sq1.com>
2017-05-15 12:51:38 -0500

    Cleaned up node files, Will be converting to use babel.

--- controllers/controller.login.js
+++ controllers/controller.login.js
@@ -1,15 +1,22 @@
 const loginModel = require('../models/model.login.js');
+const loginController = {
+  login: login
+};
 
-module.exports = {
-    login: function(req, res){
-        loginModel.login(req.body).then(function(jsonWebToken){
-            res.status(200).json({
-                'adminWebToken': jsonWebToken
-            })
-        }, function(err){
-            res.status(err.status).send({
-                'message': err.message
-            });
-        });
-    }
+//////////////////////////
+// Function Declartions //
+//////////////////////////
+
+function login(req, res){
+  loginModel.login(req.body).then(function(jsonWebToken){
+      res.status(200).json({
+          'adminWebToken': jsonWebToken
+      })
+  }, function(err){
+      res.status(err.status).send({
+          'message': err.message
+      });
+  });
 }
+
+export default loginController;
