9356849249cce3b19edc5871ce4d19907185d847
JoshSpears3 <josh.spears@sq1.com>
2017-10-08 11:45:03 -0500

    Established sequelize for ORM usage isntead of the mysql driver

--- config/database.js
+++ config/database.js
@@ -1,10 +1,31 @@
-// Require mysql for db connection.
-var mysql      = require('mysql');
-var appEnv     = require('./environments/' + (process.env.NODE_ENV || 'local') + '.json');
+const Sequelize = require('sequelize');
+const env = require('./environments/' + (process.env.NODE_ENV || 'local') + '.json');
+const sequelize = new Sequelize(
+  env.db_database,
+  env.db_username,
+  env.db_pass,
+  {
+    host: env.db_hostname,
+    dialect: 'mysql',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
+  }
+);
 
-module.exports = mysql.createConnection({
-    host     : appEnv.db_hostname,
-    user     : appEnv.db_username,
-    password : appEnv.db_pass,
-    database : appEnv.db_database
-});
+/**
+ * function connectDatabase, authenticates database
+ * connection using the configuration set above.
+ */
+async function connectDatabase(){
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (e) {
+    console.log('Unable to connect to the database:', e);
+  }
+}
+
+connectDatabase();
--- server/index.js
+++ server/index.js
@@ -7,24 +7,7 @@ const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('../webpack.config.js');
 const isDeveloping = process.env.NODE_ENV !== 'production';
 const port = isDeveloping ? 8082 : process.env.PORT;
-const databaseConfig = require('./../config/database');
 const app = express();
-
-// Database connection.
-databaseConfig.connect((err) => {
-  if (err) {
-    console.log('Error connecting to database:' + err);
-    return;
-  }
-  console.log('Database connection established.');
-});
-
-// To support JSON-encoded bodies.
-app.use(bodyParser.json());
-
-// Exporting app to use in routes file.
-module.exports.app = app;
-module.exports.databaseConfig = databaseConfig;
 
 // Run webpack middleware if in development mode.
 if (isDeveloping) {
@@ -43,6 +26,15 @@ if (isDeveloping) {
   app.use(middleware);
   app.use(webpackHotMiddleware(compiler));
 }
+
+// To support JSON-encoded bodies.
+app.use(bodyParser.json());
+
+// Exporting app to use in routes file.
+module.exports.app = app;
+
+// Require database configuration.
+require('./../config/database');
 
 // Require routes.
 require('./routes');
